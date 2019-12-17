d597d995d01b3c68ea39a75883595836417b550b
dam2a <amateu@elpuig.xeill.net>
2019-12-17 16:38:45 +0100

    sa

--- src/main/java/Factorial/Factorial.java
+++ src/main/java/Factorial/Factorial.java
@@ -1,5 +1,6 @@
 package Factorial;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class Factorial extends RecursiveTask {
@@ -9,16 +10,36 @@ public class Factorial extends RecursiveTask {
         this.n = n;
     }
 
+    private Double factSeq(){
+        System.out.println("Secuencial");
+        double temp = n;
+        for (int i = 1; i < n ; i++) {
+            temp *= i;
+        }
+        return temp;
+    }
+
+    private Double factRec() {
+        System.out.println("Recursiva");
+        Factorial task = new Factorial(n-1);
+        task.fork();
+        return n * (double) task.join();
+    }
+
     @Override
     protected Double compute(){
-        if(n < 100){
-            //Recursivo
-
+        if(n < 3){
+            return factSeq();
         }else{
-            //Sequencial
+            return factRec();
         }
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        ForkJoinPool exec = new ForkJoinPool();
+        Factorial factorial = new Factorial(170);
+        exec.invoke(factorial);
+        double result = (double) factorial.join();
+        System.out.println(result);
     }
 }
